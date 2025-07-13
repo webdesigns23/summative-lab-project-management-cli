24ced33491316e262f275ae042533aca4b3c4d58
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-12 20:08:17 -0600

    added inheritance in Users Class with a Person class, and updated README

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 ## Project Management CLI Tool
 
-Command-Line Interface (CLI) application that manages a simulated multi-user project tracker system
+Command-Line Interface (CLI) application that manages a simulated multi-user project tracker system that allows users to manage users, projects, and tasks.
+
+# Overview
+- Use CLI commands to add, view, and update these entities.
+- File Input/Output and Persists data using JSON
+
+# Features
+- View all users and add users when needed
+- View a list of projects and add new projects
+- Add new tasks and view completed tasks
+
+# Setup
+Fork and Clone:
+- Go to the provided GitHub repository link.
+- Fork the repository to your GitHub account.
+- Clone the forked repository to your local machine.
+
+Open and Run the Project:
+- Open in VSCode or your preferred IDE
+- Run the CLI tool using:
+- python cli_tool.py [command]
--- models/user.py
+++ models/user.py
@@ -1,20 +1,9 @@
 from project import Project
 
-class User:
-	all_users = []
-
+class Person:
 	def __init__(self, name, email):
 		self.name = name
 		self.email = email
-		self.projects = []
-
-		User.all_users.append(self)
-
-	def add_projects(self, project):
-		if isinstance(project, Project):
-			self.projects.append(project)
-		else:
-			raise ValueError("Invalid project")
 
 	@property
 	def name(self):
@@ -26,19 +15,36 @@ class User:
 			self._name = value
 		else:
 			raise ValueError("Name must be a string and not exceed 15 characters")
-
+		
 	def __repr__(self):
-		return f"User: {self.name}, Projects: {len(self.projects)}"
+		return f"Name: {self.name}, Email: {self.email}"
+
+class User(Person):
+	all_users = []
+
+	def __init__(self, name, email):
+		super().__init__(name, email)
+		self.projects = []
+		User.all_users.append(self)
+
+	def add_projects(self, project):
+		if isinstance(project, Project):
+			self.projects.append(project)
+		else:
+			raise ValueError("Invalid project")
 
 	def get_project_by_title(self, title):
 		for project in self.projects:
 			if project.title == title:
 				return project
 		return None
-
+	
 	@classmethod
 	def all_users(cls):
 		return cls.all_users
+	
+	def __repr__(self):
+		return f"User: {self.name}, Projects: {len(self.projects)}"
 
 
 
